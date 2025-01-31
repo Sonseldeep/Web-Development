c68fa9326afe8d3153527874994d2cef95b342a2
Sonseldeep <sonseldeep.np@gmail.com>
2025-01-31 17:09:15 +0545

    Object Destructure concept

---
+++ React Essential/DestructuringObj.js
@@ -0,0 +1,40 @@
+// 1 way
+var obj = {
+  name: "ram",
+  age: 12,
+};
+
+const { age } = obj;
+console.log(age);
+
+// another way
+var { fullName, major, mail } = {
+  fullName: "Ram Bahadur Thapa",
+  major: "CSIT",
+  mail: "xyz@gamil.com",
+};
+console.log(fullName, mail);
+
+var obj2 = {
+  name: "kakaka",
+  social: {
+    facebook: {
+      first: "ran",
+      second: "diiii",
+    },
+  },
+};
+
+// want to access second =  obj2.social.facebook
+
+const { second } = obj2.social.facebook;
+console.log(second);
+
+// method 3: index anusar naming change garan ni milxa
+
+var obj4 = [10, function () {}];
+
+var [val, def] = obj4;
+// val = 10;
+//  def = function(){} hunxa
+console.log(val);
