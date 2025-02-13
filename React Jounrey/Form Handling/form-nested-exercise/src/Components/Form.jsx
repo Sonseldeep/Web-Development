5d3bc15cfee191f7740db8eef2fd9127bff30655
Sonseldeep <sonseldeep.np@gmail.com>
2025-02-13 23:17:46 +0545

    Form Handling project

--- React Jounrey/Form Handling/form-nested-exercise/src/Components/Form.jsx
+++ React Jounrey/Form Handling/form-nested-exercise/src/Components/Form.jsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 const Form = ({ handleFormSubmitData }) => {
   const { register, handleSubmit, reset } = useForm();
   const handleFormSubmit = (data) => {
-    handleFormSubmitData(data);
+    handleFormSubmitData( );
     reset();
   };
   return (
