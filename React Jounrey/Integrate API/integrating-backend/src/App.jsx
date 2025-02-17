661ce7101af911c4a938e7c4e19293d8d95141f2
Sonseldeep <sonseldeep.np@gmail.com>
2025-02-18 00:29:35 +0545

    API fetching

--- React Jounrey/Integrate API/integrating-backend/src/App.jsx
+++ React Jounrey/Integrate API/integrating-backend/src/App.jsx
@@ -1,62 +1,18 @@
-import React, { useState } from "react";
-import axios from "axios";
-
+import Show from "./Components/Show";
+import Home from "./Components/Home";
+import { Link, Route, Routes } from "react-router-dom";
 const App = () => {
-  const [products, setProducts] = useState([]);
-  const getProducts = () => {
-    const apt = "https://fakestoreapi.com/products";
-    axios
-      .get(apt)
-      .then((products) => {
-        console.log();
-        setProducts(products.data);
-      })
-      .catch((err) => console.error(err));
-  };
-
-  const addProducts = () => {
-    const apt = "https://fakestoreapi.com/products";
-    axios
-      .post(apt, {
-        title: "test product",
-        price: 13.5,
-        description: "lorem ipsum set",
-        image: "https://i.pravatar.cc",
-        category: "electronic",
-      })
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((err) => console.error(err));
-  };
-
   return (
     <div className="pt-[5%] pl-[5%]">
-      <button onClick={getProducts} className="px-5 by-2 bg-red-300 rounded">
-        Call Product API
-      </button>
-      <br />
-      <br />
-
-      <button onClick={addProducts} className="px-5 by-2 bg-red-300 rounded">
-        Save New Product in API
-      </button>
-
-      <hr className="my-10" />
-      <ul>
-        {products.length > 0 ? (
-          products.map((p) => (
-            <li
-              key={Math.random()}
-              className="w-1/4  mb-3 p-5 bg-red-200 rounded"
-            >
-              {p.title}
-            </li>
-          ))
-        ) : (
-          <h1>Loading...</h1>
-        )}
-      </ul>
+      <nav className="flex justify-center gap-10">
+        <Link to="/">Home</Link>
+        <Link to="/show">Show</Link>
+      </nav>
+      <hr />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/show" element={<Show />} />
+      </Routes>
     </div>
   );
 };
---
+++ React Jounrey/Integrate API/integrating-backend/src/Components/Show.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+const Show = () => {
+  const [products, setProducts] = useState([]);
+  const getProducts = () => {
+    const apt = "https://fakestoreapi.com/products";
+    axios
+      .get(apt)
+      .then((products) => {
+        console.log();
+        setProducts(products.data);
+      })
+      .catch((err) => console.error(err));
+  };
+
+  return (
+    <>
+      <button onClick={getProducts} className="px-5 by-2 bg-red-300 rounded">
+        Call Product API
+      </button>
+      <hr />
+      <div>
+        <ul>
+          {products.length > 0 ? (
+            products.map((p) => (
+              <li
+                key={Math.random()}
+                className="w-1/4  mb-3 p-5 bg-red-200 rounded"
+              >
+                {p.title}
+              </li>
+            ))
+          ) : (
+            <h1>Loading...</h1>
+          )}
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Show;
