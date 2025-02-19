2b2739e3dcb68708401db3fc2f00cbfe9b602f6a
Sonseldeep <sonseldeep.np@gmail.com>
2025-02-19 23:35:54 +0545

    Context Api set up

---
+++ React Jounrey/Context API/context-api/src/Components/User.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Userdetails from "./Userdetails";
+
+const User = () => {
+  return (
+    <div>
+      <Userdetails />
+    </div>
+  );
+};
+
+export default User;
---
+++ React Jounrey/Context API/context-api/src/Components/Userdetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Userdetails = () => {
+  return <div>Userdetails</div>;
+};
+
+export default Userdetails;
