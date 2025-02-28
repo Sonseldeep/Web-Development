318744346cf0e539d77f31747b312178f7aa4103
Sonseldeep <sonseldeep.np@gmail.com>
2025-02-28 13:53:44 +0545

    Type conversion

---
+++ React Essential/TypeConversion.js
@@ -0,0 +1,21 @@
+let balance = 1000.0;
+console.log(typeof balance);
+
+console.log(typeof (5 + +"5"));
+console.log("-------------------");
+let shoppingCart = [
+  {
+    name: "Wireless Mouse",
+    price: "19.99",
+  },
+  {
+    name: "Wireless Keyboard",
+    price: "79.99",
+  },
+  {
+    name: "Monitor",
+    price: "379.99",
+  },
+];
+
+console.log(typeof shoppingCart);
